fa6dad3e38ac91f126a1558e0c293993e040c9bf
rishav-raj1 <rishavkumarraj801@gmail.com>
2022-07-10 21:50:15 +0530

    v6

--- App.js
+++ App.js
@@ -1,25 +1,84 @@
 import React from 'react';
-import {View, Text, Image, StyleSheet} from 'react-native';
+import {View, Text, Image, StyleSheet, SafeAreaView, TouchableOpacity} from 'react-native';
+import AntDesign from 'react-native-vector-icons/AntDesign';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+const Stack = createNativeStackNavigator();
 
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Main" component={Main} options={{headerShown:false}} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const Main = ({navigation}) => {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={{color: "green", fontFamily:'Roboto-MediumItalic', fontSize: 18}}>
         Rishav Kumar
       </Text>
-    </View>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')} style={{margin: 10}}>
+            <AntDesign size={30} name={'downcircle'} color="yellow" />
+          </TouchableOpacity>
+    </SafeAreaView>
   );
 };
 
+const Home = () => {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
   container: {
-    //backgroundColor: '#AAC0FA',
-    backgroundColor: 'white',
-    margin: 0,
-    borderRadius: 10,
+    backgroundColor: '#AAC0FA',
+    //backgroundColor: 'white',
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
   },
 });
 
 export default App;
+
+
+
+// // In App.js in a new project
+
+// import * as React from 'react';
+// import { View, Text } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// function HomeScreen() {
+//   return (
+//     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//       <Text>Home Screen</Text>
+//     </View>
+//   );
+// }
+
+// const Stack = createNativeStackNavigator();
+
+// function App() {
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator>
+//         <Stack.Screen name="Home" component={HomeScreen} />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// export default App;
